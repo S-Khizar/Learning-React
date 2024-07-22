aaf2c389a2727f00dbd2470f591cb31f4e93a4d4
Khizar <solkarkhizar27@gmail.com>
2024-07-22 13:28:19 +0530

    fix some issue in redux toolkit project

--- 12-reduxToolKit/src/Components/Todos.jsx
+++ 12-reduxToolKit/src/Components/Todos.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector,useDispatch } from 'react-redux'
-import {removeTodo} from '../features/todo/todoSlice'
+import {removeTodo, updateTodo} from '../features/todo/todoSlice'
 
 function Todos() {
     const todos = useSelector(state => state.todos)
@@ -35,6 +35,9 @@ function Todos() {
                 />
               </svg>
             </button>
+            <button  onClick={() => dispatch(updateTodo(todo.id,todo.text))} 
+              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+              >update</button>
           </li>
         ))}
       </ul>
